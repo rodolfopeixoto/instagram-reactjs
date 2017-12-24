1972f95589458f07bc394c65ebfb29e2b9afcbee
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-12-24 09:56:22 -0200

    Transformed HTML 5 to React

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    
+    render() {
+        return (
+            <header className="header container">
+                <h1 className="header-logo">
+                    Instagram
+                </h1>
+
+                <form className="header-busca">
+                    <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo" />
+                    <input type="submit" value="Buscar" className="header-busca-submit" />
+                </form>
+
+
+                <nav>
+                    <ul className="header-nav">
+                        <li className="header-nav-item">
+                            <a href="#">
+                                ♡
+                  {/*                 ♥ */}
+                                {/* Quem deu like nas minhas fotos */}
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>        
+        );
+    }
+}
+
+export default Header;
